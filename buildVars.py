6da2ba2b240666058f70aee7ca5a4e140d0735e7
David Parduhn <xkill85@gmx.net>
2024-04-03 10:24:17 +0200

    updated to nvda 2024.1

--- buildVars.py
+++ buildVars.py
@@ -20,14 +20,14 @@ addon_info = {
 	"addon_description" : _("""Description for the add-on.
 It can span multiple lines."""),
 	# version
-	"addon_version" : "0.11",
+	"addon_version" : "0.12",
 	# Author(s)
 	"addon_author" : u"David Parduhn <xkill85@gmx.net>",
 	# URL for the add-on documentation support
 	"addon_url" : None,
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
-	"lastTestedNVDAVersion": "2023.1"
+	"lastTestedNVDAVersion": "2024.1"
 }
 
 
